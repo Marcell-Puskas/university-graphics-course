60ac46c5ae95e75aea227a60f317457bbd0521af
Marcell <puskasmarcell05@gmail.com>
2026-03-02 22:58:17 +0100

    Lesson 4, Line editor with color picker

--- demo/line/main.c
+++ demo/line/main.c
@@ -3,19 +3,59 @@
 #include <SDL3/SDL_main.h>
 #include "line.h"
 #include <stdbool.h>
+#include "color.h"
 
 #define MAX_LINE_COUNT 500
 
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
 
-float x_mouse, y_mouse;
+static float x_mouse, y_mouse;
+static float x_line_start, y_line_start;
+static bool line_started = false;
 Line lines[MAX_LINE_COUNT];
 int line_count = 0;
-float x_line_start, y_line_start;
-bool line_started = false;
 
+Color selectabel_colors[] = {
+    COLOR_RED, COLOR_YELLOW, COLOR_GREEN, COLOR_CYAN, COLOR_BLUE, COLOR_WHITE, COLOR_BLACK
+};
+const int selectable_color_count = 7;
+int selected_color = 0;
+int _new_color = 0;
 
+#define COLOR_BUTTON_PAD 10
+static SDL_FRect color_select_rect = {
+    .h = 100,
+    .w = 100,
+};
+
+void click_line() {
+    if (!line_started) {
+        x_line_start = x_mouse;
+        y_line_start = y_mouse;
+        line_started = true;
+    } else {
+        lines[line_count++] = (Line){
+            .x1 = x_line_start,
+            .y1 = y_line_start,
+            .x2 = x_mouse,
+            .y2 = y_mouse,
+            .color = selectabel_colors[selected_color],
+        };
+        line_started = false;
+    }
+}
+
+void click_color() {
+    if (y_mouse < COLOR_BUTTON_PAD) return;
+    if (((int)x_mouse - COLOR_BUTTON_PAD) % (int)(color_select_rect.w + COLOR_BUTTON_PAD) > color_select_rect.w) return;
+    _new_color = (int)x_mouse / (int)(color_select_rect.w + COLOR_BUTTON_PAD);
+    if (_new_color >= selectable_color_count) return;
+    selected_color = _new_color;
+    SDL_Log("Selected color: %d", selected_color);
+}
+
+/* This function runs once at startup. */
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 {
     int i;
@@ -29,7 +69,6 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
         SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
-    SDL_SetRenderLogicalPresentation(renderer, 1280, 700, SDL_LOGICAL_PRESENTATION_LETTERBOX);
 
     SDL_Cursor *cursor = SDL_CreateSystemCursor(SDL_SYSTEM_CURSOR_CROSSHAIR);
     if (!cursor) {
@@ -49,34 +88,37 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
     } else if(event->type == SDL_EVENT_MOUSE_BUTTON_UP) {
         SDL_GetMouseState(&x_mouse, &y_mouse);
         SDL_Log("Mouse position: %lf; %lf", x_mouse, y_mouse);
-        if (!line_started) {
-            x_line_start = x_mouse;
-            y_line_start = y_mouse;
-            line_started = true;
+        if (y_mouse > COLOR_BUTTON_PAD + color_select_rect.h) {
+            click_line();
         } else {
-            lines[line_count++] = (Line){
-                .x1 = x_line_start,
-                .y1 = y_line_start,
-                .x2 = x_mouse,
-                .y2 = y_mouse,
-            };
-            line_started = false;
+            click_color();
         }
     }
     return SDL_APP_CONTINUE;
 }
 
 SDL_AppResult SDL_AppIterate(void *appstate)
-{
+{    
     SDL_SetRenderDrawColor(renderer, 33, 33, 33, SDL_ALPHA_OPAQUE);
     SDL_RenderClear(renderer);
-    
+
     SDL_SetRenderDrawColor(renderer, 255, 255, 0, SDL_ALPHA_OPAQUE);
     
-    for (Line *cline = lines; cline < lines + line_count; cline++)
+    for (Line *l = lines; l < lines + line_count; l++)
     {
-        SDL_RenderLine(renderer, cline->x1, cline->y1, cline->x2, cline->y2);
+         SDL_SetRenderDrawColor(renderer, l->color.r, l->color.g, l->color.b, SDL_ALPHA_OPAQUE);
+        SDL_RenderLine(renderer, l->x1, l->y1, l->x2, l->y2);
     }
+
+    for (int i = 0; i < selectable_color_count; i++)
+    {
+        Color c = selectabel_colors[i];
+        color_select_rect.x = COLOR_BUTTON_PAD + (i * (color_select_rect.w + COLOR_BUTTON_PAD));
+        color_select_rect.y = COLOR_BUTTON_PAD;
+        SDL_SetRenderDrawColor(renderer, c.r, c.g, c.b, SDL_ALPHA_OPAQUE);
+        SDL_RenderFillRect(renderer, &color_select_rect);
+    }
+    
 
     SDL_RenderPresent(renderer);
 
